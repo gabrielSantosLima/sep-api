2c3261cef1e21479a6e27f34a6727d0b9756f7e7
Gabriel Lima <bielsantoslima10@gmail.com>
2020-04-29 07:51:56 -0400

    Primeito commit

---
+++ src/test/java/com/ifam/sistema_estagio/SistemaEstagioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifam.sistema_estagio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEstagioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
